8b71150463aa9db816a5529fc4238e43057f6316
chriscrsmith <chriscs@uoregon.edu>
2023-11-06 21:38:59 +0000

    check_params.py

---
+++ check_params.py
@@ -0,0 +1,32 @@
+# checking over the command line params
+import os
+
+
+def check_params(args):
+
+    # avoid overwriting saved weights or other output files
+    if args.train == True:
+        if os.path.exists(args.out + "/mapNN_" + str(args.seed) + "_model.hdf5"):
+            print("saved model with specified output name already exists (i.e. --out)")
+            exit()
+
+    # arguments for training
+    if args.train == True:
+        if args.num_snps == None:
+            print("specify num snps via --num_snps")
+            exit()
+        if args.n == None:
+            print("specify max sample size via --n")
+            exit()
+
+    # other param combinations
+    if args.predict == True and args.empirical == None:
+        if args.num_pred != None:
+            if args.num_pred % args.batch_size != 0:
+                print(
+                    "\n\npred sets each need to be divisible by batch_size; otherwise some batches will have missing data\n\n"
+                )
+                exit()
+        if args.n == None:
+            print("missing sample size, via --n")
+            exit()
